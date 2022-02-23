cbcde4410cd7bd7b490a5d37b3b0fa3ddd4566a1
David Pires <developer.david@outlook.com>
2022-02-23 16:33:18 -0300

    navegation with params

--- src/pages/rotas/[codigo]/buscar.jsx
+++ src/pages/rotas/[codigo]/buscar.jsx
@@ -1,4 +1,6 @@
 import {useRouter} from 'next/router'
+import Link from 'next/link'
+
 export default function buscar() {
   const router = useRouter()
   const codigo = router.query.codigo
@@ -6,6 +8,7 @@ export default function buscar() {
   return (
     <div>
       <h1>Rotas | {codigo} | Buscar!!</h1>
+      
     </div>
   )
 }
