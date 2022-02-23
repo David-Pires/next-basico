5a105ec3b09416a6380524c33e7fd39097b02076
David Pires <developer.david@outlook.com>
2022-02-23 18:05:12 -0300

    get, post & postman - backend

---
+++ src/pages/api/questao/[id].js
@@ -0,0 +1,14 @@
+export default function questao(req, res) {
+  if(req.method === "GET") {
+    const id = req.query.id
+      res.status(200).json({
+        id,
+        enunciado: 'Qual é a sua cor preferida?',
+        respostas: [
+          'Branca', 'Vermelha', 'Amarela', 'Verdade'
+        ]
+      })
+  } else {
+    req.status(405).send()
+  }
+}
